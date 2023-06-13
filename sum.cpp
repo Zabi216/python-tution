9b55c2e7f9e195259f7f282e438fde53fa9148a5
zabi216 <nizamanizabiullah@gmail.com>
2023-06-13 22:10:59 +0500

    Sum of n

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int b,sum = 0;
+    cout <<"Enter a number: ";
+    cin >> b;
+        for(int i = 1; i <= b; i++){
+        
+            sum +=i;
+
+    }
+        cout << " Sum = " << sum; 
+
+    return 0;
+
+}   
