ba6bba9cd6f3140f880e2dd849fdeab5905fb35a
zabi216 <nizamanizabiullah@gmail.com>
2023-06-15 22:06:51 +0500

    Table

---
+++ multiplication.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int f;
+    cout <<"Enter a number: ";
+    cin >> f;
+    for(int i = 1; i <= 10; i++){
+
+       cout << f << " x " << i << " = " << f * i << endl;
+    
+    }
+return 0;
+
+} 
